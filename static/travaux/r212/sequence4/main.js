f31e0ab9a7f102969daab2946b20a3be0b4f12a6
atfmrmc <atfmrmc@gmail.com>
2023-05-16 21:47:35 +0200

    V4.6.0
    * Added "School" Button
    * Updated "sequence 4"

--- static/travaux/r212/sequence4/main.js
+++ static/travaux/r212/sequence4/main.js
@@ -1,10 +1,10 @@
 // 1 - je localise mon bouton effacer
 const clearButton = document.querySelector("[data-all-clear]");
-console.info(clearButton);
+// console.info(clearButton);
 
 // 2 - je surveille si on clique dessus
 clearButton.addEventListener("click", () => {
-  console.info("effacer");
+  // console.info("effacer");
 });
 
 // je localise l'affichage
@@ -13,6 +13,7 @@ const output = document.querySelector("[data-output]");
 // On défini la fonction effacer
 function clear() {
   output.innerText = "";
+  OperationCalcul = undefined;
 }
 
 clearButton.addEventListener("click", () => {
@@ -24,7 +25,7 @@ clearButton.addEventListener("click", () => {
 const numberButtons = document.querySelectorAll("[data-number]");
 
 // 2 - je regarde ce que je récupère
-console.log(numberButtons);
+// console.log(numberButtons);
 
 // On surveille le clique sur un des boutons 0-9 ou .
 numberButtons.forEach((button) => {
@@ -43,4 +44,85 @@ numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     appendNumber(button.innerText); // au clique, on appelle la fonction
   });
+});
+
+// je localise les boutons d'opération
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+// // On surveille le clique sur un des boutons
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    chooseOperation(button.innerText);
+  });
+});
+
+var OperationCalcul = undefined;
+
+// fonction qui affiche le signe de l'opération dans la zone "output"
+function chooseOperation(operation) {
+  if (
+    output.innerText.includes("-") ||
+    output.innerText.includes("+") ||
+    output.innerText.includes("*") ||
+    output.innerText.includes("/") ||
+    output.innerText.includes("%")
+  )
+    return;
+  OperationCalcul = operation;
+  output.innerText = output.innerText.toString() + "" + operation + "";
+}
+
+const equalsButton = document.querySelector("[data-equals]");
+
+equalsButton.addEventListener("click", (button) => {
+  calcul();
+});
+
+function calcul() {
+  // si rien de saisie, on arrete la le caclul
+  if (output.innerText === "") return;
+  let split = output.innerHTML.split(OperationCalcul);
+
+  // recupération des 2 valeurs
+  let nombre1 = parseFloat(split[0]);
+  let nombre2 = parseFloat(split[1]);
+
+  // verification qu'il y a bien eu des valeurs de saisies
+  if (isNaN(nombre1) || isNaN(nombre2)) return;
+
+  // console.log(nombre1);
+  // console.log(nombre2);
+  // console.log(OperationCalcul);
+
+  let moncalcul;
+
+  switch (OperationCalcul) {
+    case "+":
+      moncalcul = nombre1 + nombre2;
+      break;
+    case "-":
+      moncalcul = nombre1 - nombre2;
+      break;
+    case "*":
+      moncalcul = nombre1 * nombre2;
+      break;
+    case "/":
+      moncalcul = nombre1 / nombre2;
+      break;
+    default:
+      return;
+  }
+  //console.info(moncalcul)
+
+  output.innerText = moncalcul;
+  OperationCalcul = undefined;
+}
+// declaration de la variable à mettre en tout debut de script
+
+const switchStyleBtn = document.getElementById("switchStyleBtn");
+
+console.log(switchStyleBtn);
+
+switchStyleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("switchedStyle");
 });
